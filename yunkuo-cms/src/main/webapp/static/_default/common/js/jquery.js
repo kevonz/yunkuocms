1a52598c1b64611649e1161ba8d319baf9a18cbe
kevon zhang <kevonz@live.com>
2013-03-23 00:55:26 +0800

    update jquery reference

--- yunkuo-cms/src/main/webapp/static/_default/common/js/jquery.js
+++ yunkuo-cms/src/main/webapp/static/_default/common/js/jquery.js
@@ -60,4 +60,69 @@ BASE64={enKey:'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
  */
 (function($){function load(settings,root,child,container){$.getJSON(settings.url,{root:root},function(response){function createNode(parent){var current=$("<li/>").attr("id",this.id||"").html("<span>"+this.text+"</span>").appendTo(parent);if(this.classes){current.children("span").addClass(this.classes);};if(this.expanded){current.addClass("open");};if(this.hasChildren||this.children&&this.children.length){var branch=$("<ul/>").appendTo(current);if(this.hasChildren){current.addClass("hasChildren");createNode.call({text:"placeholder",id:"placeholder",children:[]},branch);};if(this.children&&this.children.length){$.each(this.children,createNode,[branch])}}};$.each(response,createNode,[child]);$(container).treeview({add:child});});};var proxied=$.fn.treeview;$.fn.treeview=function(settings){if(!settings||!settings.url){return proxied.apply(this,arguments);};var container=this;load(settings,"source",this,container);var userToggle=settings.toggle;return proxied.call(this,$.extend({},settings,{collapsed:true,toggle:function(){var $this=$(this);if($this.hasClass("hasChildren")){var childList=$this.removeClass("hasChildren").find("ul");childList.empty();load(settings,this.id,childList,container);};if(userToggle){userToggle.apply(this,arguments);}}}));};})(jQuery);
 
+$.extend($.validator.messages, {
+	required : "该项为必填项",
+	remote : "请修正该字段",
+	email : "请输入正确格式的电子邮件",
+	url : "请输入合法的网址",
+	date : "请输入合法的日期",
+	dateISO : "请输入合法的日期 ",
+	number : "请输入合法的数字",
+	digits : "只能输入整数",
+	creditcard : "请输入合法的信用卡号",
+	equalTo : "请再次输入相同的值",
+	accept : "请输入拥有合法后缀名的字符串",
+	maxlength : $.format("请输入一个长度最多是 {0} 的字符串"),
+	minlength : $.format("请输入一个长度最少是 {0} 的字符串"),
+	rangelength : $.format("请输入一个长度介于 {0} 和 {1} 之间的字符串"),
+	range : $.format("请输入一个介于 {0} 和 {1} 之间的值"),
+	max : $.format("该项不能大于 {0}"),
+	min : $.format("该项不能小于 {0}")
+});
 
+$.metadata.setType("attr", "vld");
+
+$.validator.AlertError = {
+	invalidHandler : function(form, validator) {
+		var errors = validator.numberOfInvalids();
+		if (errors) {
+			for (var name in validator.invalid) {
+				alert(validator.invalid[name]);
+				return;
+			}
+		}
+	},
+	showErrors : function(errors) {
+	}
+};
+$.validator.addMethod("username", function(value) {
+	var p = /^[0-9a-zA-Z\u4e00-\u9fa5\.\-@_]+$/;
+	return p.exec(value) ? true : false;
+}, "Please enter only letters,digits,chinese and '_','-','@'");
+$.validator.addMethod("path", function(value) {
+	var p = /^[0-9a-zA-Z]+$/;
+	return p.exec(value) ? true : false;
+}, "Please enter only letters and digits");
+
+$.fn.extend( {
+	showBy : function(target) {
+		var offset = target.offset();
+		var top, left;
+		var b = $(window).height() + $(document).scrollTop() - offset.top
+				- target.outerHeight();
+		var t = offset.top - $(document).scrollTop();
+		var r = $(window).width() + $(document).scrollLeft() - offset.left;
+		var l = offset.left + target.outerWidth() - $(document).scrollLeft();
+		if (b - this.outerHeight() < 0 && t > b) {
+			top = offset.top - this.outerHeight() - 1;
+		} else {
+			top = offset.top + target.outerHeight() + 1;
+		}
+		if (r - this.outerWidth() < 0 && l > r) {
+			left = offset.left + target.outerWidth() - this.outerWidth();
+		} else {
+			left = offset.left;
+		}
+		this.css("top", top).css("left", left).show();
+	}
+});
