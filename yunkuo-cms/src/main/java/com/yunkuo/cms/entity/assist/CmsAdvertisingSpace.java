6d4f872b1d9a379b688935865841b05861f00e2c
kevonz <kevonz@qq.com>
2013-08-10 21:19:58 +0800

    advertising_space

--- yunkuo-cms/src/main/java/com/yunkuo/cms/entity/assist/CmsAdvertisingSpace.java
+++ yunkuo-cms/src/main/java/com/yunkuo/cms/entity/assist/CmsAdvertisingSpace.java
@@ -2,8 +2,12 @@ package com.yunkuo.cms.entity.assist;
 
 import com.yunkuo.cms.entity.assist.base.BaseCmsAdvertisingSpace;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 
+@Entity
+@Table(name="cms_advertising_space")
 public class CmsAdvertisingSpace extends BaseCmsAdvertisingSpace {
 	private static final long serialVersionUID = 1L;
 
